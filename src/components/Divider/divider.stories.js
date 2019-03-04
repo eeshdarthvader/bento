17b3878851268b9151709dd862d727c98cefb4b8
Indrashish Ghosh <mail@indrashishghosh.com>
2019-03-04 13:38:37 +0530

    Updated divider component

--- src/components/Divider/divider.stories.js
+++ src/components/Divider/divider.stories.js
@@ -17,3 +17,17 @@ storiesOf('Divider', module)
       </div>
     </div>
   ))
+
+  .add('With text', () => (
+    <div className="cont storybook">
+      <div className="block">
+        <h3>Default divider</h3>
+        <h4>
+          Default divider has a height of 1px and a 40% opaicty of black as color
+        </h4>
+        <div className="demo">
+          <Divider text="Layover 3h"/>
+        </div>
+      </div>
+    </div>
+  ))
