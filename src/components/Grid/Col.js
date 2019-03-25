69547445085a8d097052f0e74e14de3335eee9d4
Nabeel Thurakkal <nabeelthurakkal@NabeelThurakkal.local>
2019-03-25 13:54:31 +0530

    commit grid responsive component

--- src/components/Grid/Col.js
+++ src/components/Grid/Col.js
@@ -5,6 +5,9 @@ import classNames from "classnames";
 const Col = props => {
   const colClass = classNames(
     props.span ? `col-${props.span}` : "col",
+    props.sm ? `col-${props.sm}--sm` : "",
+    props.xs ? `col-${props.xs}--xs` : "",
+    props.md ? `col-${props.md}--md` : "",
     props.offset ? `col-offset-${props.offset}` : "",
     {
       first: props.first,
@@ -18,11 +21,17 @@ const Col = props => {
 Col.propTypes = {
   span: PropTypes.number,
   offest: PropTypes.number,
-  className: PropTypes.string
+  className: PropTypes.string,
+  sm: PropTypes.number,
+  md: PropTypes.number,
+  xs: PropTypes.number,
 };
 
 Col.defaultProps = {
   span: null,
+  sm: null,
+  md: null,
+  xs: null,
   className: ''
 };
 
