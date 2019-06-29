7278b9ba05b1b62acb9f18c1a449a2f0fd6f5b14
Indrashish Ghosh <mail@indrashishghosh.com>
2019-06-29 13:42:26 +0530

    Fixed utlities typo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -52,14 +52,14 @@ const Header = props => {
                   </li>
                   <li className="flex flex-middle hover:bg-slate-dark">
                     <Link
-                      to="/components/button"
+                      to="/utilities/background"
                       className="td-none c-inherit px-32 py-24">
                       Utilities
                     </Link>
                   </li>
                   <li className="flex flex-middle hover:bg-slate-dark">
                     <Link
-                      to="/utilities/background"
+                      to="/components/button"
                       className="td-none c-inherit px-32 py-24">
                       Components
                     </Link>
