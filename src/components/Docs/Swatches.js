8cc4a2f37d866efe3448318e097c18953414c387
Indrashish Ghosh <mail@indrashishghosh.com>
2019-07-12 19:35:56 +0530

    Add click to copy to swatch

--- src/components/Docs/Swatches.js
+++ src/components/Docs/Swatches.js
@@ -1,30 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import posed from 'react-pose'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+
+const Box = posed.div({
+  hoverable: true,
+  init: { scale: 1 },
+  hover: { scale: 1.02 }
+})
+
+
 const Swatches = ({name, color, dark}) => {
+
+  const [copied, setCopyStatus] = useState(false)
+
+  const handeCopy = () => {
+    setCopyStatus(true)
+    setTimeout(() => setCopyStatus(false), 1000)
+  }
 
   const classes = classNames(
     `bg-${name}`,
-    'h-40 w-100p flex flex-middle flex-between px-12 box-border',
+    'h-40 w-100p flex flex-middle px-12 box-border fw-500 c-pointer',
     {
       'c-black': dark,
-      'c-white': !dark
+      'c-white': !dark,
+      'flex-between': !copied,
+      'flex-center': copied
     }
   )
 
   return (
-    <div className={classes}>
-      <div>
-        {name}
-      </div>
-      <div>
-        {color}
-      </div>
-    </div>
+    <CopyToClipboard
+      text={color}
+      onCopy={handeCopy}
+    >
+      <Box className={classes}>
+      {
+          copied && <div>Copied to clipboard!</div>
+      }
+        {
+          !copied &&
+          <>
+            <div>
+              {name}
+            </div>
+            <div>
+              {color}
+            </div>
+          </>
+        }
+
+      </Box>
+    </CopyToClipboard>
   );
 }
-
 
 Swatches.propTypes = {
   name: PropTypes.string.isRequired,
