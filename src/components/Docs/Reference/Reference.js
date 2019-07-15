efac0b6c0fa784e0e10b1daffc11eb7416b9a16c
Indrashish Ghosh <mail@indrashishghosh.com>
2019-07-15 18:39:08 +0530

    Update styles

--- src/components/Docs/Reference/Reference.js
+++ src/components/Docs/Reference/Reference.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import Property from './Property'
-import {TCol} from '../Table'
 
 const Reference = (props) => {
   return (
-    <div className="w-100p br-4" {...props}>
+    <div className="w-100p br-4">
       <Property class="flex flex-between p-12 bg-tertiary-100">
-        <TCol class="w-40p flex flex-start c-tertiary-500 fw-600">
+        <div class="w-40p flex flex-start c-tertiary-500 fw-600">
           Class
-        </TCol>
-        <TCol class="flex-1 flex flex-start c-tertiary-500 fw-600">
+        </div>
+        <div class="flex-1 flex flex-start c-tertiary-500 fw-600">
           Output
-        </TCol>
+        </div>
       </Property>
       {props.children}
     </div>
