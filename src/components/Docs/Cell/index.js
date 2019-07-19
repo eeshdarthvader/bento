adff9d8e85cb0a3b9015c58eef88eb5948eee1d8
Indrashish Ghosh <mail@indrashishghosh.com>
2019-07-19 17:36:46 +0530

    Added opacity docs

--- src/components/Docs/Cell/index.js
+++ src/components/Docs/Cell/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import classNames from 'classnames'
 
-const Cell = ({children, dark}) => {
+const Cell = ({ children, dark, className }) => {
 
   const cellClass = classNames(
     'c-white flex flex-middle flex-center h-40',
     {
       'bg-secondary-500': !dark,
       'bg-secondary-900': dark
-    }
+    },
+    className
   )
 
   return (
     <div className={cellClass}>
-      { children }
+      {children}
     </div>
   );
 }
