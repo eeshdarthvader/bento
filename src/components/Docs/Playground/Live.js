3024109d1f543c5d43e0861ccb5126001d52dfdd
Indrashish Ghosh <mail@indrashishghosh.com>
2019-08-08 14:19:51 +0530

    Added live component

---
+++ src/components/Docs/Playground/Live.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
+
+const Live = ({ code, scope }) => {
+  console.log(code)
+  return (
+    <div className="live">
+      <LiveProvider code={code} scope={scope}>
+        <div className="react-live-preview">
+          <LivePreview />
+        </div>
+
+        <div className="prism-code">
+          <LiveEditor />
+        </div>
+
+        <LiveError />
+      </LiveProvider>
+    </div>
+  );
+};
+
+export default Live;
---
+++ src/components/Docs/Playground/Table.js
@@ -0,0 +1,74 @@
+import React from 'react';
+
+import {
+  Field,
+  Select
+} from '@lib'
+
+const Table = () => {
+  return (
+    <>
+      <div className="propRow">
+        <div className="flex flex-middle flex-between">
+          <div>
+            <div className="flex flex-middle">
+              <p className="propName">className</p> <code>string</code>
+            </div>
+            <p className="propDescription">
+              Classes to be added to the badge component
+            </p>
+          </div>
+          <div className="propSwitcher">
+            <Field />
+          </div>
+        </div>
+      </div>
+
+      <div className="propRow">
+        <div className="flex flex-middle flex-between">
+          <div>
+            <div className="flex flex-middle">
+              <p className="propName">size</p> <code>enum</code>
+            </div>
+            <p className="propDescription">
+              These values modify the size of the component
+            </p>
+          </div>
+          <div className="propSwitcher">
+            <Select>
+              <option defaultValue>md</option>
+              <option>lg</option>
+              <option>sm</option>
+            </Select>
+          </div>
+        </div>
+      </div>
+
+      <div className="propRow">
+        <div className="flex flex-middle flex-between">
+          <div>
+            <div className="flex flex-middle">
+              <p className="propName">type</p> <code>enum</code>
+            </div>
+            <p className="propDescription">
+              Changes the appearance of the component
+            </p>
+          </div>
+          <div className="propSwitcher">
+            <Select>
+              <option defaultValue>primary</option>
+              <option>secondary</option>
+              <option>light</option>
+              <option>dark</option>
+              <option>success</option>
+              <option>warning</option>
+            </Select>
+          </div>
+        </div>
+      </div>
+
+    </>
+  );
+}
+
+export default Table;
