e023c2ff87792b797698ab35abec5ee8a73fd0c4
Indrashish Ghosh <mail@indrashishghosh.com>
2019-08-25 19:51:59 +0530

    Added installation instructions for all components

--- src/components/Docs/Sandbox/index.js
+++ src/components/Docs/Sandbox/index.js
@@ -1,10 +1,12 @@
 import Sandbox from "./Sandbox";
 import Preview from "./Preview";
+import Text from "./Text";
 import Code from "./Code";
 
 import "./sandbox.scss";
 
 Sandbox.Preview = Preview;
 Sandbox.Code = Code;
+Sandbox.Text = Text
 
 export default Sandbox;
