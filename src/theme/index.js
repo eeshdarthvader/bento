24bdd46614bb3edc01f36cd70b2e490d8bd93c71
nabeel.thurakkal <nabeel.thurakkal@cleartrip.com>
2019-09-24 12:01:12 +0530

    Menu grouping is done

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react'
-import { theme } from 'docz'
+import { theme, useDocs } from 'docz'
 
 import Helmet from 'react-helmet'
 import Sticky from 'react-stickynode'
@@ -12,8 +12,21 @@ import Pagination from './pagination'
 
 const Theme = ({children}) => {
   const TOC = generateTOC(children.props.children)
-  const menuCategory = children.props.pageContext.frontmatter.menu
+  const docCategory = children.props.pageContext.frontmatter.parent
   const docName = children.props.pageContext.frontmatter.name
+  const allDocs = useDocs()
+
+  // Get only category menus
+  const menuItems = allDocs.filter(doc => {
+    return doc.parent === docCategory
+  })
+
+  const menus = menuItems.reduce((obj, menu) => {
+    if (menu.menu === undefined) { return obj }
+    obj[menu.menu] = obj[menu.menu] || []
+    obj[menu.menu].push(menu)
+    return obj
+  }, Object.create(null))
 
   return (
     <Fragment>
@@ -25,14 +38,14 @@ const Theme = ({children}) => {
         className="flex flex-column"
         style={{ minHeight: '100vh' }}
       >
-        <Header fluid={true} activeNav={menuCategory}/>
+        <Header fluid={true} activeNav={docCategory}/>
 
         <div className="flex flex-1">
           <div
             className="nav"
             style={{ width: '260px' }}
           >
-            <Menu category={menuCategory} doc={docName} />
+            <Menu menus={menus} doc={docName} />
           </div>
 
           <div className="flex-1 bg-white elevation-md">
@@ -48,7 +61,7 @@ const Theme = ({children}) => {
                 {children}
                 <Pagination
                   docName={docName}
-                  category={menuCategory}
+                  menus={menus}
                 />
               </div>
 
--- src/theme/menu.js
+++ src/theme/menu.js
@@ -1,36 +1,36 @@
 import React from 'react';
-import { useMenus } from 'docz'
 import { Link } from 'gatsby'
 import classnames from 'classnames'
 
-const Menu = ({category, doc}) => {
-  const allMenus = useMenus()
-
-  // Get only category menus
-  const menu = allMenus.filter(menu => {
-    return menu.name === category
-  })
-
-  const menuItems = menu[0].menu
+const Menu = ({ doc, menus }) => {
+  const headings = Object.keys(menus)
   return (
-    <ul className="mx-5 p-sticky l-0" style={{top: '104px'}}>
-      {menuItems.map((menu, key) => {
-        const liClass = classnames('br-2 wc-transform', {
-          'c-tertiary-400': menu.name !== doc,
-          'hover:translate': menu.name !== doc,
-          'hover:c-tertiary-500': menu.name !== doc,
-          'hover:bg-secondary-100': menu.name === doc,
-          'bg-secondary-100': menu.name === doc,
-          'c-secondary-500': menu.name === doc
-        })
-
+    <ul className="mx-5 p-sticky l-0 pb-10" style={{ top: '104px' }}>
+      {headings.map((heading) => {
         return (
-          <li className={liClass} key={menu.id}>
-            <Link
-              to={menu.route}
-              className="c-inherit py-2 pl-2 d-block td-none fs-body c-pointer">
-              {key + 1}. {menu.name}
-            </Link>
+          <li>
+            <ul>
+              <h2 className="py-3 mt-2 c-tertiary-300 tt-uppercase ls-wide fs-caption-2">{heading}</h2>
+              {menus[heading].map((menu, key) => {
+                const liClass = classnames('br-2 wc-transform', {
+                  'c-tertiary-400': menu.name !== doc,
+                  'hover:translate': menu.name !== doc,
+                  'hover:c-tertiary-500': menu.name !== doc,
+                  'hover:bg-secondary-100': menu.name === doc,
+                  'bg-secondary-100': menu.name === doc,
+                  'c-secondary-500': menu.name === doc
+                })
+                return (
+                  <li className={liClass} key={menu.id}>
+                    <Link
+                      to={menu.route}
+                      className="c-inherit py-2 pl-2 d-block td-none fs-body c-pointer">
+                      {key + 1}. {menu.name}
+                    </Link>
+                  </li>
+                )
+              })}
+            </ul>
           </li>
         )
       })}
